f7a052edb6de464628dd8e58d840ee2df49b1411
Uriah <uriah@devexpress.com>
2020-09-30 14:05:57 +0300

    added missing files

---
+++ Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqExcersize {
+    public static class Extensions {
+        /// <summary>
+        /// Adds WHERE to a query expression
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="filter">
+        /// An array of filter caluses.
+        /// Each fitler clause consists of three objects:
+        /// 0. A property name.
+        /// 1. A value.
+        /// 2. An operation.
+        /// An operation can take the following string values: 'equal', 'not equal', 'less', 'greater', 'less or equal', 'greater or equal', 'starts with', 'ends with', 'contains'
+        /// </param>
+        /// <returns></returns>
+        public static IQueryable<T> ApplyMyFilter<T>(this IQueryable<T> query, object[][] filter) {
+            // your code goes here
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,41 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
 namespace LinqExcersize {
     class Program {
-        /// <summary>
-        /// Prints a list of contact names followed by their total amount
-        /// The total amount formula: Σ=(UnitPrice - UnitPrice * Dicount) * Quantity
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args) {
-            // your code goes here
+            IQueryable<Model> q = ReadFromFile();
+            Test(q, new[] { new[] { "ContactName", "Mario Pontes", "equal" } }, 32);
+            Test(q, new[] { 
+                new[] { "ContactName", "Paul Henriot", "equal" }, 
+                new[] { "ProductName", "Queso Cabrales", "not equal" } 
+            }, 9);
+            Test(q, new[] { new[] {"ProductName", "'s", "contains" } }, 187);
         }
-        /// <summary>
-        /// Returns an sequence of arrays
-        /// Each element in a sequence contains six values:
-        /// 0: Contact Name
-        /// 1: Product Name
-        /// 2: Order Date
-        /// 3: UnitPrice
-        /// 4: Quantity
-        /// 5: Discount
-        /// </summary>
-        /// <returns></returns>
-        static IEnumerable<object[]> ReadFromFile() {
+        static void Test(IQueryable<Model> q, object[][] filter, int expected) {
+            int actual = q.ApplyMyFilter(filter).Count();
+            var tmp = q.ApplyMyFilter(filter).ToList();
+            Debug.Assert(actual == expected, PrintFilter(filter), $"Actual value is {actual}");
+        }
+        static string PrintFilter(object[][] filter) {
+            return string.Join("and", FilterToString(filter));
+        }
+        static IEnumerable<string> FilterToString(object[][] filter) {
+            return filter.Select(c => $"({c[0]} {c[2]} {c[1]})");
+        }
+        static IQueryable<Model> ReadFromFile() {
             return File.ReadLines("data.txt")
                 .Select(l => l.Split(','))
-                .Select(a => new object[] {
-                    a[0],
-                    a[1],
-                    DateTime.Parse(a[2]),
-                    decimal.Parse(a[3]),
-                    short.Parse(a[4]),
-                    float.Parse(a[5])
-                });
+                .Select(a => new Model() {
+                    ContactName = a[0],
+                    ProductName = a[1],
+                    OrderDate = DateTime.Parse(a[2]),
+                    UnitPrice = decimal.Parse(a[3]),
+                    Quantity = short.Parse(a[4]),
+                    Discount = float.Parse(a[5])
+                }).AsQueryable();
         }
     }
 }
